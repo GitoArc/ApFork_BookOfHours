c1ccc476f2425a02077d5db12deaa58a17c60419
GitoArc <142146806+GitoArc@users.noreply.github.com>
2026-02-18 01:16:30 +0100

    rewrote lesson-skill id match

--- worlds/book_of_hours/tests/test_jsondump.py
+++ worlds/book_of_hours/tests/test_jsondump.py
@@ -3,18 +3,18 @@ from ..jsondump import lessons, skills
 
 class TestJson:
 
-    def test_lessons_match_skill(self) -> None:
-        for i in range(0,74):
-            try:
-                l = lessons[i]
-                s = skills[i]
-                ls = l["IdStr"]
-                ss = s["IdStr"]
-                lid0 = ls[1:]
-                sid0 = ss[1:]
-                assert ls[1:] == ss[1:], f"mismatch at {i} - {ls} {ss}"
-            except IndexError:
-                if lessons[i]["IdStr"] == "x.summon.echidna":
-                    assert True # echidna has no equivalent skill, only a lesson
-                else:
-                    assert False
+    def test_lessons_match_id_with_skill(self) -> None:
+        _lessons = lessons.copy()
+        _skills = skills.copy()
+        for i in range(0,73): # 73 skills in game
+            l = _lessons[0]
+            s = _skills[0]
+            ls = l["IdStr"]
+            ss = s["IdStr"]
+            assert ls[1:] == ss[1:], f"mismatch at {i} - {ls} {ss}"
+            _lessons.remove(l)
+            _skills.remove(s)
+
+        assert len(_skills) == 0
+        assert len(_lessons) == 1
+        assert _lessons[0]["IdStr"] == "x.summon.echidna", f"the last lesson, no matching skill, was not echidna."
