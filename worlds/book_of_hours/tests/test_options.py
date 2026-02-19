41ba885aee7d3377cfff81768a97a975c18c6a09
GitoArc <142146806+GitoArc@users.noreply.github.com>
2026-02-19 22:05:08 +0100

    parsing "any__any>0__useful__50" string patterns seems to work&pass test

--- worlds/book_of_hours/tests/test_options.py
+++ worlds/book_of_hours/tests/test_options.py
@@ -1,15 +1,10 @@
 from .bases import BoHTestBase
+from ..GenericFilter import parseFilterstring
+from ..jsondump import everything
 from ..options import BoHOptions
 
 
 class TestOptions(BoHTestBase):
-
-    def test_every_option_has_display_name(self):
-        options:BoHOptions = self.world.options
-        members = vars(options)
-        for s in members:
-            op = getattr(options, s)
-            self.assertTrue(getattr(op, "display_name") != "", f"{s} has no display_name set")
 
     # more a "compile-time" test (if the option does not exist, I'll know before test runs)
     def test_world_options_not_None(self):
@@ -20,3 +15,41 @@ class TestOptions(BoHTestBase):
         assert ops.insoulnity is not None
         assert ops.terrainsanity is not None
         pass
+
+
+    def test_every_option_has_display_name(self):
+        options:BoHOptions = self.world.options
+        members = vars(options)
+        for s in members:
+            op = getattr(options, s)
+            self.assertTrue(getattr(op, "display_name") != "", f"{s} has no display_name set")
+
+
+    def test_parse_filterstring(self):
+        lines = [
+            "__all<2__filler__50",
+            "persistent__all<2__progression__5",
+            "persistent__all<3__useful__10",
+            "persistent__all<4__useful__10",
+            "hindsight,salt__any>0__progression+useful__30"
+        ]
+        for line in lines:
+            passed = []
+            filterObj = parseFilterstring(line)
+            for e in everything:
+                if filterObj.evaluate(e):
+                    passed.append(e)
+            # now assert what 'filterObj.evaluate' has filtered (without using .evaluate()):
+            for p in passed:
+                # manually check each stat without .evaluate()
+                # ; That did 'a->c', now use 'b->c' to check if 'a' set 'c' correctly
+                targetStringFound = False
+                for t in filterObj.targets:
+                    targetStringFound = targetStringFound or p.contains_substr(t)
+                self.assertTrue(targetStringFound, f"{p} was not a valid target but got caught")
+
+                predicateMatchedAspects = filterObj.filterPredicate.evaluate_dict(p.Aspects)
+                predicateMatchedTerrainReqs = filterObj.filterPredicate.evaluate_dict(p.Requires)
+
+                self.assertTrue(predicateMatchedAspects or predicateMatchedTerrainReqs, f"{p} was incorrectly passed")
+            pass
